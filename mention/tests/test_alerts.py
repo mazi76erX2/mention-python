d5a62801f4b5751fefe63f4c4a17229c53466b11
Xolani Mazibuko <mazi76erx@gmail.com>
2024-07-04 21:13:06 +0200

    Updated settings for linting

--- mention/tests/test_alerts.py
+++ mention/tests/test_alerts.py
@@ -1,4 +1,4 @@
-"""Alerts tests"""
+"""Test suite for the alerts module."""
 
 from typing import Any, Dict
 from unittest.mock import MagicMock
@@ -56,151 +56,133 @@ def advanced_alert_data() -> Dict[str, Any]:
     }
 
 
-def test_create_basic_alert_success(
-    mock_oauth_session: OAuth2Session, basic_alert_data: Dict[str, Any]
-) -> None:
-    """
-    Tests the successful creation of a basic alert.
+class TestCreateAnAlertAPI:
+    """Test suite for the CreateAnAlertAPI class."""
 
-    Verifies that the API call is made with the correct parameters and returns the expected response.
-    """
-    api = CreateAnAlertAPI(
-        mock_oauth_session,
-        "account123",
-        "Test Basic Alert",
-        basic_alert_data,
-        ["en"],
-        noise_detection=True,
-    )
-    response = api.query()
-    mock_oauth_session.post.assert_called_once_with(
-        f"{api._base_url}/accounts/account123/alerts",
-        json={
-            "name": "Test Basic Alert",
-            "query": basic_alert_data,
-            "languages": ["en"],
-            "noise_detection": "true",
-        },
-    )
-    assert response == {"alert_id": "12345"}
+    def test_create_basic_alert_success(
+        self, mock_oauth_session: OAuth2Session, basic_alert_data: Dict[str, Any]
+    ) -> None:
+        """Tests the successful creation of a basic alert."""
+        api = CreateAnAlertAPI(
+            mock_oauth_session,
+            "account123",
+            "Test Basic Alert",
+            basic_alert_data,
+            ["en"],
+            noise_detection=True,
+        )
+        response = api.query()
 
+        mock_oauth_session.post.assert_called_once_with(
+            f"{api._base_url}/accounts/account123/alerts",
+            json={
+                "name": "Test Basic Alert",
+                "query": basic_alert_data,
+                "languages": ["en"],
+                "noise_detection": "true",
+            },
+        )
 
-def test_create_advanced_alert_success(
-    mock_oauth_session: OAuth2Session, advanced_alert_data: Dict[str, Any]
-) -> None:
-    """
-    Tests the successful creation of an advanced alert.
-
-    Verifies that the API call is made with the correct parameters and returns the expected response.
-    """
-    api = CreateAnAlertAPI(
-        mock_oauth_session,
-        "account123",
-        "Test Advanced Alert",
-        advanced_alert_data,
-        ["en", "fr"],
-        countries=["US", "FR"],
-    )
-    response = api.query()
-    mock_oauth_session.post.assert_called_once_with(
-        f"{api._base_url}/accounts/account123/alerts",
-        json={
-            "name": "Test Advanced Alert",
-            "query": advanced_alert_data,
-            "languages": ["en", "fr"],
-            "countries": ["US", "FR"],
-        },
-    )
-    assert response == {"alert_id": "12345"}
+        assert response == {"alert_id": "12345"}
 
 
-def test_create_alert_with_optional_params(
-    mock_oauth_session: OAuth2Session, basic_alert_data: Dict[str, Any]
-) -> None:
-    """
-    Tests alert creation with all optional parameters.
+    def test_create_advanced_alert_success(
+        self, mock_oauth_session: OAuth2Session, advanced_alert_data: Dict[str, Any]
+    ) -> None:
+        """Tests the successful creation of an advanced alert."""
+        api = CreateAnAlertAPI(
+            mock_oauth_session,
+            "account123",
+            "Test Advanced Alert",
+            advanced_alert_data,
+            ["en", "fr"],
+            countries=["US", "FR"],
+        )
+        response = api.query()
 
-    Verifies that the API call is made with the correct parameters including optional fields.
-    """
-    api = CreateAnAlertAPI(
-        mock_oauth_session,
-        "account123",
-        "Alert with Options",
-        basic_alert_data,
-        ["en"],
-        sources=["web", "twitter"],
-        blocked_sites=["example.com"],
-        noise_detection=False,
-        reviews_pages=["amazon.com"],
-    )
-    response = api.query()
+        mock_oauth_session.post.assert_called_once_with(
+            f"{api._base_url}/accounts/account123/alerts",
+            json={
+                "name": "Test Advanced Alert",
+                "query": advanced_alert_data,
+                "languages": ["en", "fr"],
+                "countries": ["US", "FR"],
+            },
+        )
 
-    mock_oauth_session.post.assert_called_once_with(
-        f"{api._base_url}/accounts/account123/alerts",
-        json={
-            "name": "Alert with Options",
-            "query": basic_alert_data,
-            "languages": ["en"],
-            "sources": ["web", "twitter"],
-            "blocked_sites": ["example.com"],
-            "noise_detection": "false",
-            "reviews_pages": ["amazon.com"],
-        },
-    )
-    assert response == {"alert_id": "12345"}
+        assert response == {"alert_id": "12345"}
 
 
-def test_create_alert_http_error(
-    mock_oauth_session: OAuth2Session, basic_alert_data: Dict[str, Any]
-) -> None:
-    """
-    Tests the behavior of the API when an HTTP error occurs during alert creation.
+    def test_create_alert_with_optional_params(
+        self, mock_oauth_session: OAuth2Session, basic_alert_data: Dict[str, Any]
+    ) -> None:
+        """Tests alert creation with all optional parameters."""
+        api = CreateAnAlertAPI(
+            mock_oauth_session,
+            "account123",
+            "Alert with Options",
+            basic_alert_data,
+            ["en"],
+            sources=["web", "twitter"],
+            blocked_sites=["example.com"],
+            noise_detection=False,
+            reviews_pages=["amazon.com"],
+        )
+        response = api.query()
 
-    Verifies that the API returns an empty dictionary in case of an HTTP error.
-    """
-
-    mock_oauth_session.post.side_effect = HTTPError("Simulated HTTP Error")
-    api = CreateAnAlertAPI(
-        mock_oauth_session, "account123", "Error Alert", {}, ["en"]
-    )  # Empty query
-    response = api.query()
-    assert response == {}  # Expect an empty dictionary on error
-
-
-def test_create_alert_empty_query(
-    mock_oauth_session: OAuth2Session, basic_alert_data: Dict[str, Any]
-) -> None:
-    """
-    Tests the behavior of the API when an empty query is provided.
-    """
-    api = CreateAnAlertAPI(
-        mock_oauth_session,
-        "account123",
-        "Error Alert",
-        {},  # Empty query data
-        ["en"],
-    )
-
-    response = api.query()
-
-    # Check if the error was printed
-    assert response == {}
+        mock_oauth_session.post.assert_called_once_with(
+            f"{api._base_url}/accounts/account123/alerts",
+            json={
+                "name": "Alert with Options",
+                "query": basic_alert_data,
+                "languages": ["en"],
+                "sources": ["web", "twitter"],
+                "blocked_sites": ["example.com"],
+                "noise_detection": "false",
+                "reviews_pages": ["amazon.com"],
+            },
+        )
+        assert response == {"alert_id": "12345"}
 
 
-def test_create_alert_invalid_language(
-    mock_oauth_session: OAuth2Session, basic_alert_data: Dict[str, Any]
-) -> None:
-    """
-    Tests the behavior of the API when an invalid language code is provided.
+    def test_create_alert_http_error(
+        self, mock_oauth_session: OAuth2Session, basic_alert_data: Dict[str, Any]
+    ) -> None:
+        """Tests handling of HTTP errors during alert creation."""
+        mock_oauth_session.post.side_effect = HTTPError("Simulated HTTP Error")
+        api = CreateAnAlertAPI(
+            mock_oauth_session, "account123", "Error Alert", {}, ["en"]
+        )  # Empty query
+        response = api.query()
+        assert response == {}  # Expect an empty dictionary on error
 
-    Verifies that the API raises a ValueError exception if an invalid language code is passed.
-    """
-    with pytest.raises(ValueError):
-        CreateAnAlertAPI(
+    def test_create_alert_empty_query(
+        self, mock_oauth_session: OAuth2Session, basic_alert_data: Dict[str, Any]
+    ) -> None:
+        """Tests behavior when creating an alert with an empty query."""
+        api = CreateAnAlertAPI(
             mock_oauth_session,
             "account123",
             "Error Alert",
-            basic_alert_data,
-            ["invalid_lang_code"],  # Invalid language code
-            noise_detection=True,
+            {},  # Empty query data
+            ["en"],
         )
+
+        response = api.query()
+
+        assert response == {}  # Expect an empty dictionary on error
+
+
+    def test_create_alert_invalid_language(
+        self, mock_oauth_session: OAuth2Session, basic_alert_data: Dict[str, Any]
+    ) -> None:
+        """Tests behavior when creating an alert with an invalid language code."""
+        with pytest.raises(ValueError):
+            CreateAnAlertAPI(
+                mock_oauth_session,
+                "account123",
+                "Error Alert",
+                basic_alert_data,
+                ["invalid_lang_code"],  # Invalid language code
+                noise_detection=True,
+            )
